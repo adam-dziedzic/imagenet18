e30213da961273a209c6637f7aea1c4c96cf64d5
Adam Dziedzic <adam.cajf@gmail.com>
2022-07-31 08:20:20 -0400

    run

--- training/run_imagenet.sh
+++ training/run_imagenet.sh
@@ -17,4 +17,4 @@ python -m torch.distributed.launch \
 --nproc_per_node=4 --nnodes=1 --node_rank=0 \
 train_imagenet_nv.py \
 --workers=4 --fp16 --logdir /ssd003/home/ady/imagenet_fast --distributed --init-bn0 --no-bn-wd \
---phases "[{'ep': 0, 'bs': 256}, {'ep': (0, 8), 'lr': (0.5, 1.0)}, {'ep': (8, 15), 'lr': (1.0, 0.125)}, {'ep': 15, 'bs': 112, 'min_scale': 0.087}, {'ep': (15, 25), 'lr': (0.22, 0.022)}, {'ep': (25, 28), 'lr': (0.022, 0.0022)}, {'ep': 28, 'bs': 64, 'min_scale': 0.5, 'rect_val': True}, {'ep': (28, 29), 'lr': (0.00125, 0.000125)}]" --skip-auto-shutdown
+--phases "[{'ep': 0, 'sz': 128, 'bs': 512}, {'ep': (0, 7), 'lr': (1.0, 2.0)}, {'ep': (7, 13), 'lr': (2.0, 0.25)}, {'ep': 13, 'sz': 224, 'bs': 224, 'min_scale': 0.087}, {'ep': (13, 22), 'lr': (0.4375, 0.043750000000000004)}, {'ep': (22, 25), 'lr': (0.043750000000000004, 0.004375)}, {'ep': 25, 'sz': 288, 'bs': 128, 'min_scale': 0.5, 'rect_val': True}, {'ep': (25, 28), 'lr': (0.0025, 0.00025)}]" --skip-auto-shutdown
